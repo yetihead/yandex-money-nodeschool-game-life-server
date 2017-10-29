62705e6185432f85b0397b4a564dee7bc88cc29e
yetihead <yetibrain@yandex.ru>
2017-10-29 18:13:59 +0300

    add websocket on socket io

--- src/main.js
+++ src/main.js
@@ -1,61 +1,59 @@
 'use strict';
 
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 const qs = require('query-string');
 const LifeGame = require('../lib/LifeGameVirtualDom');
 const randomHEXColor = require('random-hex-color');
 
 const game = new LifeGame();
-const wss = new WebSocket.Server({
-	port: 3000,
-	verifyClient: ({req}, done) => {
-		const {token} = qs.parse(req.url.substr(2));
-		if (!token) {
-			done(false, 401, 'There is no token!');
-		}
-		req.__token = token;
-		done(true);
-	}
+const io = new SocketIO(3000, {
+	path: '/',
+	transports: ['websocket']
 });
 
-wss.on('connection', (ws, {__token}) => {
+io.use((ws, next) => {
+	if (ws.handshake.query && ws.handshake.query.token) {
+		return next();
+	}
+	// тут я так и не понял как установить 401 статус
+	next(new Error('Authentication error'));
+});
 
-	ws.on('message', msg => {
-		const {type, data} = JSON.parse(msg);
+io.on('connection', ws => {
+	let token = ws.client.request._query.token;
+	ws
+		.on('message', messageHandler)
+		.on('error', errorHandler)
+		.send(initUser(token));
+});
 
-		if (type === 'ADD_POINT' && data) {
-			game.applyUpdates(data);
-			return;
-		}
-		
-		console.warn('unhandled message type: ', type, JSON.stringify(data, null, ' '));
-	});
+game.sendUpdates = data => {
+	io.emit('message', JSON.stringify({ type: 'UPDATE_STATE', data }));
+};
 
-	ws.on('error', err => {
-		console.warn('error in socket: ', err);
-	});
+function messageHandler(msg) {
+	const {type, data} = JSON.parse(msg);
+	if (type === 'ADD_POINT' && data) {
+		game.applyUpdates(data);
+		return;
+	}
+	console.warn('unhandled message type: ', type, JSON.stringify(data, null, ' '));
+}
 
-	ws.send(JSON.stringify({
+function errorHandler (err) {
+	console.warn('error in socket: ', err);
+}
+
+function initUser (token) {
+	return JSON.stringify({
 		type: 'INITIALIZE',
 		data: {
 			state: game.state,
 			settings: game.settings,
-			user: {
-				token: __token,
-				color: randomHEXColor()
-			}
-		}
-	}));
-});
-
-game.sendUpdates = data => {
-	wss.clients.forEach((client) => {
-		if (client.readyState === WebSocket.OPEN) {
-			client.send(JSON.stringify({ type: 'UPDATE_STATE', data }));
+			user: {token, color: randomHEXColor()}
 		}
 	});
-};
-
+}
 
 //
 // YOUR CODE GOES HERE...
